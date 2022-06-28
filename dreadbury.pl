ebb71c84bf57039b200949ef1805467a538e94a7
folaciregui <102688272+folaciregui@users.noreply.github.com>
2022-06-28 15:18:44 -0300

    Update dreadbury.pl

--- dreadbury.pl
+++ dreadbury.pl
@@ -1,5 +1,5 @@
 
-mataA(Persona,Victima) :-
+matoA(Persona,Victima) :-
     odiaA(Persona,Victima),
     not(esMasRicoQue(Persona,Victima)).
 
@@ -21,3 +21,43 @@ esMasRicoQue(Persona, tiaAgatha) :-
     viveEnLaMansion(Persona),
     not(odiaA(mayordomo,Persona)),
     Persona \= tiaAgatha.
+
+/* PUNTO 1
+?- matoA(Alguien, tiaAgatha).
+Alguien = charles ;
+false.
+*/
+
+/* PUNTO 2
+
+- Si existe alguien que odie a milhouse.
+?- odiaA(_, milhouse).
+false.
+
+- A quién odia charles.
+?- odiaA(charles, Odiado).
+Odiado = tiaAgatha ;
+Odiado = mayordomo ;
+false.
+
+- El nombre de quien odia a tía Ágatha.
+?- odiaA(Odiador, tiaAgatha).
+Odiador = charles ;
+false.
+
+- Todos los odiadores y sus odiados.
+odiaA(Odiador, Odiado).
+Odiador = charles,
+Odiado = tiaAgatha ;
+Odiador = charles,
+Odiado = mayordomo ;
+Odiador = tiaAgatha,
+Odiado = charles ;
+Odiador = mayordomo,
+Odiado = charles.
+
+- Si es cierto que el mayordomo odia a alguien.
+?- odiaA(mayordomo, _).
+true.
+
+*/
